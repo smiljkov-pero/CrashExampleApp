20e23a2d42a242842624efe247f34a20a653c6e4
Pero Smiljkov <pero.smiljkov@deliveryhero.com>
2021-01-27 14:27:06 +0100

    Initial commit

---
+++ app/src/main/java/com/pero/crashexampleapp/ExampleApplication.kt
@@ -0,0 +1,13 @@
+package com.pero.crashexampleapp
+
+import android.app.Application
+
+class ExampleApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        var s: String? = null
+        s!!.length
+    }
+}
